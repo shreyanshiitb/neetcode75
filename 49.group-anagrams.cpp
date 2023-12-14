02ed08d7a91afc4586b6acc7f81540fa4b3c28bd
Shreyansh Jain <sjain0615@gmail.com>
2023-12-14 13:33:39 +0530

    Add solution

--- 49.group-anagrams.cpp
+++ 49.group-anagrams.cpp
@@ -8,18 +8,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    vector<vector<string>> groupAnagrams(vector<string>& strs) {
-
+    vector<vector<string>> groupAnagrams(vector<string> &strs)
+    {
         // hash map from string idx to string's char count
-        unordered_map<int, unordered_map<char,int>> m;
+        unordered_map<int, unordered_map<char, int>> m;
 
         int n = strs.size();
-        for(int i=0;i<n;i++){
+        for (int i = 0; i < n; i++)
+        {
             string &currStr = strs[i];
-            unordered_map<char,int> currStrCount;
-            for(char &c:currStr){
+            unordered_map<char, int> currStrCount;
+            for (char &c : currStr)
+            {
                 currStrCount[c]++;
             }
             m[i] = currStrCount;
@@ -28,32 +31,40 @@ public:
         vector<vector<string>> ans;
         unordered_set<int> vis;
 
-        for(int i=0;i<n;i++){
-
-            if(vis.count(i)){
+        for (int i = 0; i < n; i++)
+        {
+            if (vis.count(i))
+            {
                 continue;
             }
             vector<string> currGroup;
             vis.insert(i);
             currGroup.push_back(strs[i]);
 
-            for(int j=i+1;j<n;j++){
+            for (int j = i + 1; j < n; j++)
+            {
 
-                if(!vis.count(j) && strs[i].size()==strs[j].size()){
+                if (!vis.count(j) && strs[i].size() == strs[j].size())
+                {
                     bool isAnagram = true;
-                    for(auto &[k,v]:m[i]){
-                        if(!m[j].count(k) || m[j][k] != v){
+                    for (auto &[k, v] : m[i])
+                    {
+                        if (!m[j].count(k) || m[j][k] != v)
+                        {
                             isAnagram = false;
                             break;
                         }
                     }
-                    for(auto &[k,v]:m[j]){
-                        if(!m[i].count(k) || m[i][k] != v){
+                    for (auto &[k, v] : m[j])
+                    {
+                        if (!m[i].count(k) || m[i][k] != v)
+                        {
                             isAnagram = false;
                             break;
                         }
                     }
-                    if(isAnagram){
+                    if (isAnagram)
+                    {
                         vis.insert(j);
                         currGroup.push_back(strs[j]);
                     }
@@ -65,4 +76,3 @@ public:
     }
 };
 // @lc code=end
-
